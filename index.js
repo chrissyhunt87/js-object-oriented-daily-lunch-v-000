bcab8094a6efcd145ddecfc10b99440ea0ca2d1f
Chrissy Hunt <chrissy.hunt@gmail.com>
2018-05-17 17:41:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,3 +32,15 @@ class Meal{
     store.meals.push(this);
   }
 }
+
+let deliveryId = 0;
+class Delivery{
+  constructor(mealId, neighborhoodId, customerId){
+    this.mealId = mealId;
+    this.neighborhoodId = neighborhoodId;
+    this.customerId = customerId;
+    this.id = ++deliveryId;
+    
+    store.deliveries.push(this);
+  }
+}
